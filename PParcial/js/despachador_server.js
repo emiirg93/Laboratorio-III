87035b35c21ff5b87e9752a23018d2bbd6940bc8
Emiliano Rago <emiliano.rg93@gmail.com>
2019-07-09 23:05:48 -0300

    Primer Parcial

---
+++ PParcial/js/despachador_server.js
@@ -0,0 +1,16 @@
+var xhr;
+function traerPersonas() {
+
+
+}
+
+function guardarPersona(persona) {
+   
+  }
+
+function eliminarPersona(id) {
+ }
+
+function modificarPersona(persona) {
+
+}
---
+++ PParcial/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#abrirModal').click(function () {
+        $('#modal').show(1000);
+    })
+
+    $('.close').click(function () {
+        $('#modal').hide(1000);
+    })
+})
+
+function CargarTabla(Personas) {
+
+}
